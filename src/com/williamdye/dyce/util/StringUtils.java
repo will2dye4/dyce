454d06872b09986dff69ae89fbbfa35aa7387027
William <will2dye4@me.com>
2013-05-22 23:55:25 -0400

    More null checking.

--- src/com/williamdye/dyce/util/StringUtils.java
+++ src/com/williamdye/dyce/util/StringUtils.java
@@ -3,8 +3,10 @@ package com.williamdye.dyce.util;
 public class StringUtils
 {
 
-    public static String join(String[] array, String separator)
+    public static String join(String[] array, String separator) throws IllegalArgumentException
     {
+        if (separator == null)
+            throw new IllegalArgumentException("Separator may not be null!");
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < array.length; i++) {
             if (array[i] != null)
